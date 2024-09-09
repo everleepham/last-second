7686c741ce51fc34c487a615fc88b157f45e2be4
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2024-09-09 16:22:26 +0200

    second commit

---
+++ src/main/java/com/pham/lastsecond/dto/FlightBookingDTO.java
@@ -0,0 +1,15 @@
+package com.pham.lastsecond.dto;
+
+import java.time.LocalDateTime;
+
+public class FlightBookingDTO {
+    private int flightId;
+    private String customerFname;
+    private String customerLname;
+    private int customerId;
+    private String departAirport;
+    private LocalDateTime departDate;
+    private String arrivalAirport;
+    private LocalDateTime arrivalDate;
+    private double price;
+ }
---
+++ src/main/java/com/pham/lastsecond/service/FlightBookingService.java
@@ -0,0 +1,25 @@
+package com.pham.lastsecond.service;
+
+
+
+public class FlightBookingService {
+    public void creatFlightBooking() {
+
+    }
+
+    public void getFlightById() {
+
+    }
+
+    public void getAllFlights() {
+
+    }
+
+    public void updateFlight() {
+
+    }
+
+    public void deleteFlight() {
+
+    }
+}
