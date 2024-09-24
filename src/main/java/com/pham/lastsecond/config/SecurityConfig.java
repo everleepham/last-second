b380ecfcc844a99078c97e16db504efe3815ce81
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2024-09-24 15:30:25 +0200

    update

--- src/main/java/com/pham/lastsecond/config/SecurityConfig.java
+++ src/main/java/com/pham/lastsecond/config/SecurityConfig.java
@@ -2,25 +2,23 @@ package com.pham.lastsecond.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(httpBasic -> {
-                    // Configure HTTP Basic settings here (if needed)
-                })
-                .requiresChannel(channel -> channel
-                        .anyRequest().requiresSecure()
+                        .anyRequest().permitAll()
                 );
 
         return http.build();
